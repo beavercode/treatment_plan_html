e4a430911167eb6cf908823d8c6f082ac9cdcb7f
bbr <erobober@gmail.com>
2015-04-27 20:15:09 +0300

    App skeleton. Todo: sesseions, pdf, docx, right markup.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,14 +1,13 @@
 <?php
-
 namespace UTI;
+
+require 'vendor/autoload.php';
 
 use UTI\Core\Router;
 use UTI\Core\AppException;
 use UTI\Core\System;
 
-require 'vendor/autoload.php';
-
-ini_set('display_errors', 1);
+ini_set('display_errors', 1);   //disable for prod
 ini_set('short_open_tag', 1);
 
 try {
@@ -25,18 +24,8 @@ try {
     $router = new Router($_SERVER, URI_BASE);
     $router->run();
 
-    //https://github.com/squizlabs/PHP_CodeSniffer/wiki
-    //https://www.jetbrains.com/phpstorm/help/using-php-code-sniffer-tool.html
-    //http://phpmd.org/
-    //https://jtreminio.com/2012/10/composer-namespaces-in-5-minutes/
-    //http://harikt.com/blog/2015/01/13/experimenting-on-different-framework/
-
-    //https://ru.wikipedia.org/wiki/Model-View-Controller
-    //http://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93controller
-    //http://habrahabr.ru/post/150267/
-    //http://habrahabr.ru/post/215605/
-
 } catch (AppException $e) {
+    //enable for prod
     //System::log(APP_LOG . 'exceptions.log', $e->getError());
     echo $e->getError();
 } catch (\Exception $e) {
